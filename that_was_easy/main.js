a5890f95315bc4068da3f0a13536e209d48a9ba1
Akash Jain <10014285@sbstudents.org>
2018-03-06 01:28:28 +0000

    Added key binding to the That Was Easy button file

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -2,4 +2,14 @@ function sayThatWasEasy(){
     var thatWasEasy = new Audio("that_was_easy.mp3");
     thatWasEasy.play();
 }
+/*function delegateKeyPress(event){
+    //console.log(event.charCode);
+    if (event.charCode == 32){
+        //var thatWasEasy = new Audio("that_was_easy.mp3");
+        //thatWasEasy.play();
+        $("#easy").trigger("click");
+    }
+
+}
 $("#easy").on("click", sayThatWasEasy);
+$(document).keypress(delegateKeyPress);*/
